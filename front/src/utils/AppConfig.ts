dea971d8994554ff28ad6ae538d178be210c21e9
MajesticSoup <davenlu1989@gmail.com>
2022-11-10 15:45:38 +0900

    change discord invite to permanent

--- front/src/utils/AppConfig.ts
+++ front/src/utils/AppConfig.ts
@@ -5,7 +5,7 @@ export const AppConfig = {
   locale: 'en',
   site_links: {
     external: {
-      discord: 'https://discord.gg/V8aEUvQB',
+      discord: 'https://discord.com/invite/kqhzghtcds',
       github: 'https://github.com/TokyoPython',
       facebook: 'https://www.facebook.com/groups/TokyoPythonSocietyClub',
       slack: 'https://tokyo-python-sc.slack.com',
